7529a45f777703ca6d04c815ffb0e2d00c240f31
ketkikk <kulketki7@gmail.com>
2022-05-25 11:26:40 +0530

    first commit

---
+++ uniquearrayitem/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace uniquearrayitem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] items = { 1, 5, 9, 2, 5, 9, 8, 10 };
+            int n = items.Length;
+
+            Console.WriteLine("Unique array elements: ");
+
+            for (int i = 0; i < n; i++)
+            {
+                bool isDuplicate = false;
+                for (int j = 0; j < i; j++)
+                {
+                    if (items[i] == items[j])
+                    {
+                        isDuplicate = true;
+                        break;
+                    }
+                }
+
+                if (!isDuplicate)
+                {
+                    Console.WriteLine(items[i]);
+
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
